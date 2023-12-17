690b516d75c93ced97ed3adfe57af23c71dcb814
ydhyoxly <bachurina.2008@mail.ru>
2023-12-17 11:58:35 +0300

    I fixed it a little bit

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 import pygame
 from random import randint
 
+from start_window.StartWindow import StartMenu
+
+
 if __name__ == '__main__':
     pygame.init()
+    StartMenu()
+    running = True
     while running:
-        start_main_window(pygame)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False

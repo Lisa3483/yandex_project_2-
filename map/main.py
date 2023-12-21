ad1a49ab8750b59fefe545f48f5985785d9483e1
Lisa3483 <liz.shtodina@mail.ru>
2023-12-21 17:40:10 +0300

    добавлена карта и возможность перемещения по ней

---
+++ map/main.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+# author: "Jake b" , created: [2011/8/]
+# more tiles at: C:\Users\jake\Documents\python\boilerplate-2011\jor\02. tiling maps\1. basic map tiles\images
+
+from game import Game
+
+if __name__ == "__main__": 
+    game = Game()    
+    game.main_loop()
